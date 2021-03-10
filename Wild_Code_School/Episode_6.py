88f88a41f45f2a408a7698d77ef5578901b75880
Vladimir <vladimir.rav@gmail.com>
2021-03-10 23:07:39 +0000

    Episode 6 : Du vin à boire ! (data)

---
+++ Wild_Code_School/Episode_6.py
@@ -0,0 +1,27 @@
+'''
+Dans l'éditeur de code ci-dessous, calculez la quantité d'eau consommée depuis le début du voyage :
+
+1. Le navire est parti avec 1000 litres d'eau.
+2. 390 litres ont été consommés depuis le départ.
+3. Chaque Argonaute consomme 2L par jour (il y a 50 Argonautes)
+Calculez le nombre de jours restant avant que l'équipage ne soit à court d'eau.
+
+Trouvez la bonne équation pour faire ce calcul avec Python.
+
+Ensuite, affichez les résultats à l'aide de la fonction print
+'''
+litres_eau = 1000
+litres_consommes = 390
+num_argonautes = 50
+jours = round((litres_consommes / 50) / 2)
+jours_restant = round(((litres_eau - litres_consommes) / 50) / 2)
+
+print(
+  "Litres d'eau: " + str(litres_eau),
+  "Litres consommés: " + str(litres_consommes),
+  "Argonautes: " + str(num_argonautes),
+  "L'eau restant: " + str(litres_eau - litres_consommes),
+  "Jours: " + str(jours),
+  "Jours restant: " + str(jours_restant),
+  sep="\n"
+)
--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-# Commentaire en Python
-print(
-  "Résister et continuer",
-  "https://argo.wildcodeschool.com/expeditions/precourse/quests/1171",
-  sep="\n"
-)
-print("-" * 65)
-print(
-"Argonautes !\nHissons nos voiles avec enthousiasme et courage vers Colchide !\nAllons récupérer la toison d'or des griffes du dragon !\nNous gagnerons, avec ou sans sandales."
-)
+from Wild_Code_School import Episode_6 as e6
