696c5c1a8ac22e0f99b0c139913d2ae5a84741e6
Vladimir <vladimir.rav@gmail.com>
2021-03-10 22:33:06 +0000

    Épisode 5 : Un peu de coaching (data)

---
+++ main.py
@@ -0,0 +1,10 @@
+# Commentaire en Python
+print(
+  "Résister et continuer",
+  "https://argo.wildcodeschool.com/expeditions/precourse/quests/1171",
+  sep="\n"
+)
+print("-" * 65)
+print(
+"Argonautes !\nHissons nos voiles avec enthousiasme et courage vers Colchide !\nAllons récupérer la toison d'or des griffes du dragon !\nNous gagnerons, avec ou sans sandales."
+)
